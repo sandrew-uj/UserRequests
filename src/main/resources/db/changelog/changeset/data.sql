b4950d0ff671b72cf63d764b7a1efe188deebea9
Andrei_Smirnov <sandrew2003@gmail.com>
2024-03-14 13:50:08 +0300

    admin password changed

--- src/main/resources/db/changelog/changeset/data.sql
+++ src/main/resources/db/changelog/changeset/data.sql
@@ -6,7 +6,7 @@ INSERT INTO roles (name) VALUES
 INSERT INTO users (username, password) VALUES
     ('Andrew Smirnov', '$2a$10$DFDectT2.InN1O3vJA8m5ukEJuoz3HY98LOu51nWh8IHSbL8arXpG'),
     ('USER', '$2a$10$QT4DYML.0uJW8Tnx1zMsAuD6TatCcu1mCWgJI8vGp2/yBJwWimCj6'),
-    ('ADMIN', '$2a$10$TxgGUF0Oe4.nZsh1i3uaeO8kbt6S.XE/Z6YmItSRUU1EJNZohbftO '),
+    ('ADMIN', '$2a$10$0Hq9E.iRz4jxu/kPZW1z2Ol2QsuzlCp0V.hksUbVIGDUinAM/WjpG'),
     ('OPERATOR', '$2a$10$f5bBr3dA29WUaTch3TS5V.vAiB0WdnavaTKJzXAwN3v7.n3m9RNMa');
 
 INSERT INTO user_roles (user_id, role_id) VALUES
